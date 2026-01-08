c52a19e1581b37c01b0f5c7483154b56f9095ad2
Martin Lawrence <mlawrence06@gmail.com>
2026-01-07 17:16:07 -0700

    mv to production

--- extension/scripts/config.js
+++ extension/scripts/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
     // API_BASE_URL: "http://localhost:8081/api", // If using SSH Tunnel
-    API_BASE_URL: "http://192.168.1.12:8081/api", // Direct LAN access to Merlin
+    API_BASE_URL: "https://shadowpulse.live/api",
     STATS_URL: "https://shadowpulsev2.b-cdn.net/stats.json",
     POLLING_INTERVAL: 2000,
     FLASH_COOLDOWN: 5000
